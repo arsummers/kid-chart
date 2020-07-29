8a46222bc83d391e66bc975e1f1a9c0c579af255
arsummers <aliyasummers1@gmail.com>
2020-07-29 16:57:34 -0700

    rules CRUD

--- chart/urls.py
+++ chart/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('kids/<int:pk>/update/', views.KidUpdate.as_view(), name='kid-update'),
     path('kids/<int:pk>/delete/', views.KidDelete.as_view(), name='kid-delete'),
     path('rules/', views.RuleListView.as_view(), name='rules'),
-    path('rules/<int:pk>', views.RuleDetailView.as_view(), name='rule-detail')
+    path('rules/<int:pk>', views.RuleDetailView.as_view(), name='rule-detail'),
+    path('rule/create/', views.RuleCreate.as_view(), name='rule-create'),
+    path('rules/<int:pk>/update', views.RuleUpdate.as_view(), name='rule-update'),
+    path('rules/<int:pk>/delete', views.RuleDelete.as_view(), name='rule-delete'),
 ]
